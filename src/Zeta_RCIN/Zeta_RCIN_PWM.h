06a1007f4850f711860ab2a061cdfe0164ff816f
Pradhyu18 <onlypradhyuman@gmail.com>
2024-06-27 18:46:10 +0530

    updated the pwm input

---
+++ src/Zeta_RCIN/Zeta_RCIN_PWM.h
@@ -0,0 +1,31 @@
+#ifndef Zeta_RCIN_H
+#define Zeta_RCIN_H
+#include <Arduino.h>
+#include <param.h>
+#include <vector>
+
+class ZetaRCIN_PWM
+{
+    public:
+        // define functions that will be called during interrupts
+        void handleInterrupt1();
+        void handleInterrupt2();
+        void handleInterrupt3();
+        void handleInterrupt4();
+        void handleInterrupt5();
+        void handleInterrupt6();
+        //function to compute the calculate pwm signal value
+        void common_for_calculation(int pin);
+        // function to get the value of the channel
+        std::vector<int> get_PWM_Value(void);
+        
+        // adding constructor to initaialzie values and inturrupt functios
+        ZetaRCIN_PWM();
+
+    private:
+        unsigned long timer[6];
+        int last_channel[6];
+        int input[6];
+        std::vector<int> Receiver_Values;
+};
+#endif// Zeta_RCIN_H
--- src/param.h
+++ src/param.h
@@ -5,10 +5,17 @@
 #define MPU6050_ADDRESS 0x68
 
 //RC PPM Parameters
-#define RC_PPM_PIN 1
-#define USE_PPM
+// #define RC_PPM_PIN 1
+// #define USE_PPM
 //use the below macro in case of pwm input
-//#define USE_PWM
+#define USE_PWM
+#define RC_PWM_PIN 1
+#define RC_PWM_PIN 2
+#define RC_PWM_PIN 3
+#define RC_PWM_PIN 4
+#define RC_PWM_PIN 5
+#define RC_PWM_PIN 6
+
 
 
 // Zeta Buffer Parameters
